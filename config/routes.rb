4d76e375e8e21e394f3e22d371411bf08d05fc54
github username <github email address>
2019-07-23 18:59:10 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :articles
+
+  get 'articles/:id', to: 'articles#show', as: 'article'
+  get 'articles/new', to: 'articles#new', as: 'new_article'
+
+  
 end
